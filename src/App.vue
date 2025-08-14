5f84bc080ec622e8dacae1a37d693d5731c13db9
Voke <google@voke.com>
2025-08-14 18:30:10 +0800

    initialization

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <div class="globalheader">
+    <nav class="globalheader"></nav>
+    <div id="globalnav-placeholder" class="globalnav-placeholder"></div>
+  </div>
+  <nav class="os-localnav"></nav>
+  <main>
+    <section class="section-welcome text-center bg-gradient-to-b from-[#ffffff] to-[#f5f5f7]">
+      <div class="py-9">
+        <div class="w-[87.5%] mx-auto">
+          <div>
+            <h1 class="header-eyebrow">iOS 26 預覽</h1>
+            <h2 class="mt-1.5 text-[32px] font-semibold relative">
+              面貌一新
+              <span class="absolute">，</span>
+              <br />上手更奇妙<span class="absolute">。</span>
+            </h2>
+          </div>
+          <div class="mt-5">
+            <p class="text-[#6e6e73]">秋季推出</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
